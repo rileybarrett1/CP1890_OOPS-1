5ecda78b811426c0d33d3d5cce41cf354b593788
arun-rameshbabu <140411791+arun-rameshbabu@users.noreply.github.com>
2024-01-10 08:59:21 -0330

    Exercises for Class 1

---
+++ Working_With_Numbers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 09:53:39 2024
+
+@author: arun.rameshbabu
+"""
+
+import math as m
+
+# To the power of
+maximum_value = m.pow(2, 31)
+maximum_value2 = (2**31)
+
+# Sqrt
+square_root = m.sqrt(16)
+
+# Floor and Ceil
+res = m.floor(12.845)
+res2 = m.ceil(12.345)
+
