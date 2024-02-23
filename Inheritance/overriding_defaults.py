163b5115f4cbced0a471ec660d8ade6558dbaab9
arun-rameshbabu <140411791+arun-rameshbabu@users.noreply.github.com>
2024-02-23 09:42:43 -0330

    Class 17 - Some Exercises

--- Inheritance/overriding_defaults.py
+++ Inheritance/overriding_defaults.py
@@ -9,33 +9,33 @@
 #
 # print(product1 == product2)
 
-# from Dice_Roller_Classes import Die, Dice
+from Dice_Roller_Classes import Die, Dice
+
+dice = Dice()
+dice.addDie(Die(6))
+dice.addDie(Die(3))
+dice.addDie(Die(2))
+
+for obj in dice:
+    # die.roll()
+    print(obj)
+
+# from Subclasses import CustomRequestError
 #
-# dice = Dice()
-# dice.addDie(Die(6))
-# dice.addDie(Die(3))
-# dice.addDie(Die(2))
+# def read_movies():
+#     try:
+#         movies = []
+#         with open('movies.csv') as file_movies:
+#             pass
+#     except FileNotFoundError:
+#         raise CustomRequestError('This is my error')
+#     except Exception:
+#         raise CustomRequestError('This is a generic error')
 #
-# for die in dice:
-#     die.roll()
-#     print(str(die))
-
-from Subclasses import CustomRequestError
-
-def read_movies():
-    try:
-        movies = []
-        with open('movies.csv') as file_movies:
-            pass
-    except FileNotFoundError:
-        raise CustomRequestError('This is my error')
-    except Exception:
-        raise CustomRequestError('This is a generic error')
-
-
-
-try:
-    movies = read_movies()
-except CustomRequestError as e:
-    print("CustomRequestError:", e)
+#
+#
+# try:
+#     movies = read_movies()
+# except CustomRequestError as e:
+#     print("CustomRequestError:", e)
 
